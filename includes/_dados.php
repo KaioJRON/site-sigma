7047651ee343a6eaef55a64c39e290147c929756
KaioJRON <kaiooliveski@gmail.com>
2022-06-09 12:22:42 -0300

    commit1

--- includes/_dados.php
+++ includes/_dados.php
@@ -1,7 +1,15 @@
 <?php
 
 $produtos = array(
-    0 => array('nome' =>'capivara','preco' => '2000,00 R$', 'descricao'=>'capivara frau','imagem'=>'capivara2.jpg'),
-
+    0 => array('nome' =>'NFT macaco1','preço' => '2000,00 R$', 'descricao'=>'','imagem'=>'nft macaco1.jpg'),
+    1 => array('nome' =>'NFT macaco2','preço' => '1000,00 R$', 'descricao'=>'','imagem'=>'nft macaco2.webp'),
+    2 => array('nome' =>'','preço' => '1500,00 R$', 'descricao'=>'','imagem'=>''),
+    3 => array('nome' =>'','preço' => '2300,00 R$', 'descricao'=>'','imagem'=>''),
+    4 => array('nome' =>'','preço' => '1700,00 R$', 'descricao'=>'','imagem'=>''),
+    5 => array('nome' =>'','preço' => '2800,00 R$', 'descricao'=>'','imagem'=>''),
+    6 => array('nome' =>'','preço' => '3000,00 R$', 'descricao'=>'','imagem'=>''),
+    7 => array('nome' =>'','preço' => '3500,00 R$', 'descricao'=>'','imagem'=>''),
+    8 => array('nome' =>'','preço' => '3400,00 R$', 'descricao'=>'','imagem'=>''),
+    9 => array('nome' =>'','preço' => '4000,00 R$', 'descricao'=>'','imagem'=>'')
 );
-?>
+?> 
--- index.php
+++ index.php
@@ -3,9 +3,15 @@
 include_once './includes/_dados.php';
 include_once './includes/_head.php';
 include_once './includes/_header.php';
+$id = $_GET['id'];
 ?>
 
 <h1>Home</h1>
+
+<?php
+f<?php echo $produtos[$id]["imagem"];?>">
+?>
+
 
 <?php
 // include do footer
